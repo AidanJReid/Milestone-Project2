8193177b9842659c5e0a5bde0e58e246633e1d47
Ubuntu <ubuntu@ip-172-31-22-53.ec2.internal>
2019-09-17 15:10:48 +0000

    updated with older d3 version

--- js/main.js
+++ js/main.js
@@ -5,14 +5,14 @@ queue()
 function makeGraphs(error, ufoData) {
     var ndx = crossfilter(ufoData);
     var city_dim = ndx.dimension(dc.pluck('city'));
-    var state = ndx.dimension(dc.pluck('state'));
+    var total_duration = city_dim.group().reduceSum(dc.pluck('duration'));
 
     dc.barChart("#data1")
         .width(300)
-        .height(500)
+        .height(150)
         .margins({top: 10, right: 50, bottom: 30, left: 50 })
         .dimension(city_dim)
-        .group(state)
+        .group(total_duration)
         .transitionDuration(500)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
