ffc96a7c6dcbd1a997806fff1f67f26811e7d717
Ubuntu <ubuntu@ip-172-31-22-53.ec2.internal>
2019-09-17 13:53:39 +0000

    wrestling with data

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
-d3.csv("assets/data/nuforc.csv").then(makeGraphs);
+queue()
+    .defer(d3.json, "assets/data/ufo.json")
+    .await(makeGraphs);
 
-function makeGraphs(ufoData) {
-    
+function makeGraphs(error, ufoData) {
     var ndx = crossfilter(ufoData);
-    
-    ufoData.forEach(function (d) {
+    var city_dim = ndx.dimension(dc.pluck('city'));
+    var state = ndx.dimension(dc.pluck('state'));
+
+    dc.barChart("#data1")
+        .width(300)
+        .height(500)
+        .margins({top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(city_dim)
+        .group(state)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("City")
+        .yAxis().ticks(4);
+
+    dc.renderAll();
 }
